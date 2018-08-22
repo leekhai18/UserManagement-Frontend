fd89b8d4644c405d376cb0fead0a7be3db0aeeeb
Lee Khai <leekhai18@gmail.com>
2018-08-22 17:23:33 +0700

    add +, -, and observable

--- app/viewmodels/create.js
+++ app/viewmodels/create.js
@@ -1,6 +1,20 @@
-define(['knockout', './user', 'knockout.validation'], function (ko, User){
+define(['knockout', 'jquery', 'knockout.validation'], function (ko, $){
     ko.validation.init();
-    return {
-       
-    };
+    
+    var ProfileModel = function (){
+        var self = this;
+
+        self.groups = ko.observableArray([{nameGroup: ""}]);
+
+        self.addGroup = function (){
+            self.groups.push({nameGroup: ""});
+        };
+
+        self.removeGroup = function (group){
+            console.log(self.groups());
+            self.groups.remove(group);
+        };
+    }
+    
+    return new ProfileModel();
 });
