68bee9b52681fef7e3a3107104c0fe03e9cfd5d5
BuiNguyenHoangAnh <32384308+BuiNguyenHoangAnh@users.noreply.github.com>
2018-08-22 15:30:54 +0700

    update manage page

--- app/viewmodels/list.js
+++ app/viewmodels/list.js
@@ -1,6 +1,21 @@
 define(['knockout', './user', 'knockout.validation'], function (ko, User){
     ko.validation.init();
+
+    var firstName = ko.observable(),
+    	lastName = ko.observable(),
+    	role = ko.observable(),
+    	group = ko.observable(),
+    	organization = ko.observable(),
+    	image = ko.observable();
+
     return {
+       firstName: 'Anh',
+       lastName: 'Bui',
+       role: 'Developer',
+       group: 'Frontend',
+       organization: 'UIT',
+       image: image,
+
        
     };
 });
