d88740d4eeae7592ba0b0d9cddec716d27b88dad
mtSiniChi <minhtan.itdev@gmail.com>
2018-08-28 09:31:19 +0700

    Run with simple data

--- app/viewmodels/list.js
+++ app/viewmodels/list.js
@@ -1,6 +1,9 @@
 define(['knockout', 'plugins/router', 'knockout.validation'], function (ko, router){
     //list of users
-    var lUsers = ko.observableArray([]);
+    var lUsers = ko.observableArray([
+        {firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'UIT', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"},
+        {firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'University of Information Technology', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"}
+        ]);
 
     //Info of user
     function userInfo(data) {
@@ -13,21 +16,20 @@ define(['knockout', 'plugins/router', 'knockout.validation'], function (ko, rout
         self.image = ko.observable(data.image);
     }
 
-    // function activate(data) {
-    //     $.getJSON("/lUsers", function(allData) {
-    //         var mappedUsers = $.map(allData, function(item) { return new userInfo(item) });
-    //         console.log(lUsers);
-    //         self.lUsers(mappedUsers);
-    //     });
-    // }
+    function activate(data) {
+        $.getJSON("/lUsers", function(allData) {
+            var mappedUsers = $.map(allData, function(item) { return new userInfo(item) });
+            console.log(lUsers);
+            self.lUsers(mappedUsers);
+        });
+    }
 
     return {
         // Array of users
-        // users : ko.observableArray([
-        //     {firstName, lastName, role, group, organization, image},
-       	// 	// {firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'UIT', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"},
-       	// 	// {firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'University of Information Technology', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"}
-       	// 	]),
+        users : ko.observableArray([
+       		{firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'UIT', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"},
+       		{firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'University of Information Technology', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"}
+       		]),
        	
         // gotoCreate: gotoCreate,
         lUsers: lUsers,
