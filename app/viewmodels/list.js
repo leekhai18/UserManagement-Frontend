a3cf7d9abb786f6fff5374f2aa25393046644703
mtSiniChi <minhtan.itdev@gmail.com>
2018-08-28 14:50:52 +0700

    Loaded list users from server

--- app/viewmodels/list.js
+++ app/viewmodels/list.js
@@ -1,45 +1,50 @@
-define(['knockout', 'plugins/router', 'knockout.validation'], function (ko, router){
-    //list of users
-    var lUsers = ko.observableArray([
-        {firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'UIT', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"},
-        {firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'University of Information Technology', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"}
-        ]);
+define(['knockout', 'plugins/http', 'plugins/router', 'knockout.validation'],
+    function (ko, http, router) {
+        //list of users
+        var lUsers = ko.observableArray([]);
 
-    //Info of user
-    function userInfo(data) {
-    	var self = this;
-    	self.firstName = ko.observable(data.firstName);
-    	self.lastName = ko.observable(data.lastName);
-    	self.role = ko.observable(data.role);
-    	self.group = ko.observable(data.group);
-        self.organization = ko.observable(data.organization);
-        self.image = ko.observable(data.image);
-    }
+        //Info of user
+        function userInfo(data) {
+            var self = this;
+            self.firstName = ko.observable(data.firstName);
+            self.lastName = ko.observable(data.lastName);
+            self.role = ko.observable(data.role);
+            self.group = ko.observable(data.group);
+            self.organization = ko.observable(data.organization);
+            self.image = ko.observable(data.image);
+        }
 
-    // function activate(data) {
-    //     $.getJSON("/lUsers", function(allData) {
-    //         var mappedUsers = $.map(allData, function(item) { return new userInfo(item) });
-    //         console.log(lUsers);
-    //         self.lUsers(mappedUsers);
-    //     });
-    // }
+        var addUser = function () {
+            // console.log("Add User");
+            router.navigate("create");
+        }
 
-    var addUser = function() {
-        console.log("sadfasd");
-        router.navigate("create");
+        var getAllUsers = function () {
 
-    }
+            //clear
+            lUsers.removeAll();
 
-    return {
-        // Array of users
-        users : ko.observableArray([
-       		{firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'UIT', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"},
-       		{firstName: 'Anh', lastName: 'Bui', role: 'Developer', group: 'Frontend', organization: 'University of Information Technology', image: "https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=50"}
-       		]),
-       	
-        // gotoCreate: gotoCreate,
-        lUsers: lUsers,
-        addUser: addUser,
-        // activate: activate,
-    };
-});
+            // use plugin HTTP of Durandaljs
+            http.get('http://localhost:16567/api/user')
+                .then(function (u) {
+
+                    console.log(u);
+
+                    u.forEach(element => {
+                        lUsers.push(element);
+                    });
+
+                });
+        }
+
+        return {
+
+            activate: function () {
+                // console.log('Activate Page');
+                getAllUsers();
+            },
+
+            lUsers: lUsers,
+            addUser: addUser,
+        };
+    });
